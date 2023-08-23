45e9ad0a66e515a2ce17658e89417a9b0180c385
Jason M <jason.morta23@gmail.com>
2023-08-23 19:44:45 +0200

    updates

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./style.css"
+
+export default function Login() {
+  return (
+       
+     <container class="box-container">
+        <div class="box-container-inner">
+          <h1>Log in</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem quam quidem rem similique dolore maiores eos
+            doloribus. Amet fuga dolores nostrum saepe aliquid incidunt soluta quaerat quibusdam, voluptate impedit quisquam?
+          </p>
+        </div>
+      </container>
+  )
+}
